5a22c35d19a7f376b751d81f2fdab680ab14a7d1
yang <59786677@qq.com>
2019-05-16 00:10:58 +0800

    修改test_a_pic.sh

--- test_a_pic.sh
+++ test_a_pic.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-
+#
 PIC_NAME="000046.png"
-WEIGHTS="./backup/yolov3-voc_900.weights"
+# 权重路径
+WEIGHTS="./backup/yolov3-voc.backup"
+
+
 PICTURE="/Data1/Data_DL/KITTI/JPEGImages/${PIC_NAME}"
 ./darknet detector test cfg/voc.data cfg/yolov3-voc.cfg ./backup/yolov3-voc.backup ${PICTURE}
 
